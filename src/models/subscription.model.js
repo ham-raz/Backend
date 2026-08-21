d4615a4210d82de3997d2c48a16a4ad489ecb353
ham-raz <243577@students.au.edu.pk>
2026-08-21 18:38:22 +0500

    Add getWatchHistroy endpoint

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-const subsciptionSchema = new Schema(
+const subscriptionSchema = new Schema(
   {
     subscriber: {
       type: Schema.Types.ObjectId,
@@ -13,4 +13,4 @@ const subsciptionSchema = new Schema(
   { timestamps: true }
 );
 
-export const Subsciption = mongoose.model("Subsciption", subsciptionSchema);
+export const Subsciption = mongoose.model("Subscription", subscriptionSchema);
