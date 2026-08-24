99412590e953a62eea3659adec74aa11a7cd88c3
ham-raz <243577@students.au.edu.pk>
2026-08-24 16:17:36 +0500

    Implement dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -2,16 +2,68 @@ import mongoose from "mongoose";
 import { Video } from "../models/video.model.js";
 import { Subscription } from "../models/subscription.model.js";
 import { Like } from "../models/like.model.js";
-import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const channelId = req.user._id;
+
+  const totalVideos = await Video.countDocuments({
+    owner: channelId,
+  });
+
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: channelId,
+  });
+
+  const videoStats = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalViews: {
+          $sum: "$views",
+        },
+      },
+    },
+  ]);
+
+  const totalViews = videoStats[0]?.totalViews || 0;
+
+  const videoIds = await Video.find({
+    owner: channelId,
+  }).distinct("_id");
+
+  const totalLikes = await Like.countDocuments({
+    video: {
+      $in: videoIds,
+    },
+  });
+
+  const stats = {
+    totalVideos,
+    totalSubscribers,
+    totalViews,
+    totalLikes,
+  };
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, stats, "Channel stats fetched successfully"));
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-  // TODO: Get all the videos uploaded by the channel
+  const videos = await Video.find({
+    owner: req.user._id,
+  }).sort({ createdAt: -1 });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "Channel videos fetched successfully"));
 });
 
 export { getChannelStats, getChannelVideos };
