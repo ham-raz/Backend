d1b76a4afd1e98273ce136da41a282daba7370cf
ham-raz <243577@students.au.edu.pk>
2026-08-25 15:17:40 +0500

    Update cookie options for secure handling based on environment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -125,7 +125,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   };
 
   return res
@@ -160,7 +161,8 @@ const logoutUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   };
 
   return res
@@ -196,9 +198,9 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
     const options = {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     };
-
     const { accessToken, newRefreshToken } =
       await generateAccessAndRefereshTokens(user._id);
 
