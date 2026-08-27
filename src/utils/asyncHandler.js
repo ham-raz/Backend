953e6a65b3acc6511103f47a9defc94ae7b60182
ham-raz <243577@students.au.edu.pk>
2026-08-27 14:46:20 +0500

    Refactor app structure and cleaning up asyncHandler implementation

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,18 +1,17 @@
-const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-  };
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//   };
+// };
+
+import { ApiError } from "./ApiError.js";
+
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json(new ApiError(error.code, error.message));
+  }
 };
 
 export { asyncHandler };
-
-// const asyncHandler = (fn) => async (req, res, next) => {
-//   try {
-//     await fn(req, res, next);
-//   } catch (error) {
-//     res.status(err.code || 500).json({
-//       success: false,
-//       message: err.message,
-//     });
-//   }
-// };
